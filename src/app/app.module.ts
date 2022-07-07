b6d7712b72a1ee341273d87d73ab481090afe519
Sree1710 <kunjusree817@gmail.com>
2022-07-07 19:10:06 +0530

    search places component created and tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
+import { SearchplacesComponent } from './searchplaces/searchplaces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRegistrationComponent
+    TravelRegistrationComponent,
+    SearchplacesComponent
   ],
   imports: [
     BrowserModule,
